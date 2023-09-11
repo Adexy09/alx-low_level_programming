def794a92c006911c0187ef4a1578f73f2bc4eea
Adexy09 <aderantifunminiyirelaoded@gmail.com>
2023-09-10 17:40:27 -0700

    data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints the size of various coputer data types
+ * Return: o when successful
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long d;
+	float f;
+
+printf("Size of a char: %lu bytes(s)\n"; (unsigned long)sizeof(a));
+printf("Size of a int: %lu bytes(s)\n"; (unsigned long)sizeof(b));
+printf("Size of a long int: %lu bytes(s)\n"; (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu bytes(s)\n"; (unsigned long)sizeof(d));
+printf("Size of a float: %lu bytes(s)\n"; (unsigned long)sizeof(f));
+return (0);
+}
