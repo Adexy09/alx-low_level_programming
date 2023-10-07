8f1dd627fc7c88bae1714abe5922aa8a13887177
Adexy09 <aderantifunminiyirelaoded@gmail.com>
2023-10-07 16:06:55 -0700

    standard library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *s);
+{
+	(void)s;
+}
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
