1911cfc2b9d6aefb043fb9231b06245bec07d992
Adexy09 <aderantifunminiyirelaoded@gmail.com>
2023-10-11 17:14:54 -0700

    0x0f.C function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,41 +1,18 @@
-#include <stdio.h>
-#include "function_pointers.h"
+0-print_name.c
 
+#include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - Prints a given name using a function
- * @name: The name to be printed
- * @f: A function pointer to a printing function
- */
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-	/* Check if the name is not NULL and the function is not NULL */
-	if (name != NULL && f != NULL)
-	{
-		/* Call the function pointed to by f and pass the name */
-		f(name);
-	}
-}
+	if (name == NULL || f == NULL)
+		return;
 
-/**
- * print_name_to_console - Prints a name to the console
- * @name: The name to be printed
- */
-void print_name_to_console(char *name)
-{
-	/* Check if the name is not NULL */
-	if (name != NULL)
-	{
-		printf("My name is %s.\n", name);
-	}
-}
-
-int main(void);
-{
-	char myName[] = "John";
-
-	/* Print the name using the print_name function */
-	print_name(myName, print_name_to_console);
-
-	return (0);
+	f(name);
 }
 
